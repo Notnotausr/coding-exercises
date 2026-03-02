111a6a2a7a08ea9c0d27a52bca3a3a36254a75f4
Emma <emmafu0225@gmail.com>
2026-03-01 17:51:34 -0800

    week 1 for emma

---
+++ Java/Week 1/emma/CargoPlanner.java
@@ -0,0 +1,32 @@
+public class CargoPlanner {
+    public static void main(String[] args) {
+        String name = "VeryCoolShip";
+        // The name of the ship is "VeryCoolShip".
+        System.out.println("Welcome aboard the " + name + "! Let's plan our cargo for the upcoming space mission.");
+        double distance = 54.6;
+        // The distance to the destination is 54.6 million kilometers.
+        System.out.println("The distance required to reach the destination is " + distance + " million kilometers.");
+        double fuelLevel = 75.5;
+        // The ship's fuel level is currently at 75.5%.
+        System.out.println("The ship's fuel level is currently at " + fuelLevel + "%.");
+        int astronauts = 7;
+        double astronautsfood = 1.5;
+        // The amount of food each astronaut needs per day is 1.5 kilograms.
+        boolean missionSuccessful = true;
+        boolean missionFailed = false;
+        // The mission is successful if the ship reaches its destination, otherwise it fails.
+        System.out.println("Your mission has been successful: " + missionSuccessful);
+        System.out.println("Your mission has failed: " + missionFailed);
+        System.out.println("The ship just used 12.2% of its fuel.");
+        double fuelLevelAfterUse = 63.3;
+        // The ship's fuel level is now at 63.3%.
+        System.out.println("The ship's fuel level is now " + fuelLevelAfterUse + "%.");
+        double distanceDividedBy3 = 18.2;
+        // The distance per leg is 18.2 million kilometers.
+        System.out.println("The ship has " + distanceDividedBy3 + " million kilometers left to go.");
+        double passengerCapacity = astronauts * astronautsfood;
+        // The ship's passenger capacity is 10.5 kilograms per day.
+        System.out.println("The ship's passenger capacity is " + passengerCapacity + " kilograms per day.");
+        
+    }
+}
---
+++ Java/Week 1/emma/StudentGradeCalculator.java
@@ -0,0 +1,16 @@
+public class StudentGradeCalculator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Student Grade Calculator! Please enter your name.");
+        String name = "Emma";
+        System.out.println("Please enter five tests scores of the course." + name + ", here are your test scores:");
+        int score1 = 85;
+        int score2 = 90;
+        int score3 = 81;
+        int score4 = 88;
+        int score5 = 92;
+        System.out.println("Calculating your average score...");
+        double averageScore = (score1 + score2 + score3 + score4 + score5) / 5.0;
+        System.out.println("Emma, your average score is: " + averageScore);
+    }
+}
+    
