08c0bd46f41eb4104c0d052e23c1e1ebb559150e
Kerrie Liu <kerrieliu00@gmail.com>
2026-03-17 15:56:20 -0700

    added grading comments

--- Java/Week 2/Siyuan/Book.java
+++ Java/Week 2/Siyuan/Book.java
@@ -8,9 +8,13 @@ public class Book {
         this.author = bookAuthor;
         this.pages = numPages;
     }
+
+    /* requires a describe() method that prints a sentence describing the book, including the name, author, and number of pages */
+    
     public void read() {
         System.out.println("You are reading: " + title + " by " + author);
     }
+    
     public void bookmarkPage(int page) {
         if (page > 0 && page <= pages) {
             System.out.println("You have bookmarked page " + page + " of " + title);
@@ -18,6 +22,9 @@ public class Book {
             System.out.println("Invalid page number. This book has " + pages + " pages.");
         }
     }
+
+    /* requires a modifier method to modify the page count of a book and print updated number of pages */
+    
     public static void main(String[] args) {
         Book myBook = new Book("harry potter", "jk rowling", 500);
         Book myBook2 = new Book("lord of the rings", "tolkien", 400);
