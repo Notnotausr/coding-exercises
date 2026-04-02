42ccc590e46090814f1ddce1f192dbfe55c61db9
Siyuan Chen <mr.siyuan.chen@outlook.com>
2026-04-01 21:21:34 -0700

    added fizzbuzz

--- Java/Week3/Siyuan/Fizzbuzz/Fizzbuzz.java
+++ Java/Week3/Siyuan/Fizzbuzz/Fizzbuzz.java
@@ -1,1 +1,21 @@
-public class Fizzbuzz
+public class Fizzbuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } 
+            
+            if (i % 5 == 0) {
+                System.out.println("Jazz");
+            } 
+            
+            if (i % 7 == 0) {
+                System.out.println("Buzz");
+            }
+            
+            if (i % 3 != 0 && i % 5 != 0 && i % 7 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
