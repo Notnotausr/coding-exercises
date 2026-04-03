f3c94abca10b20b88ce6aed4a38d87b37ab7e9af
Kerrie Liu <kerrieliu00@gmail.com>
2026-04-02 17:57:03 -0700

    Update Fizzbuzz.java

--- Java/Week3/Siyuan/Fizzbuzz.java
+++ Java/Week3/Siyuan/Fizzbuzz.java
@@ -1,6 +1,29 @@
 public class Fizzbuzz {
     public static void main(String[] args) {
         for (int i = 1; i <= 1000; i++) {
+            /* i would've done it in a way that it would concatenate the strings if they were supposed to be printed, then println it after
+            for example:
+            
+            String string = "";
+            if (i % 3 == 0) {
+                string += "Fizz";
+            }
+            
+            if (i % 5 == 0) {
+                string += "Jazz";
+            } 
+            
+            if (i % 7 == 0) {
+                string += "Buzz";
+            }
+            
+            if (i % 3 != 0 && i % 5 != 0 && i % 7 != 0) { // more efficient to use string.equals("")
+                System.out.println(i);
+            } else {
+                System.out.println(string);
+            }
+            */
+            
             if (i % 3 == 0) {
                 System.out.println("Fizz");
             } 
