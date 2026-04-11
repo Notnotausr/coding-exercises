7e944169682f8d2550b51efcc7094d274e3de7f9
Kerrie Liu <kerrie.liu@rollingrobotics.org>
2026-04-11 16:58:28 -0700

    moved files

--- Java/Week5/Siyuan/NestedLoop.java
+++ Java/Week5/Siyuan/NestedLoop.java
@@ -1,15 +1,20 @@
-public class NestedLoop{
+public class NestedLoop {
     public static void main(String[] args) {
-        int[][] grid = {
+        int[][] twoDArray = {
             {1, 2, 3},
-            {4, 5, 6}
+            {4, 5, 6} 
         };
         int sum = 0;
-        for (int i = 0; i < grid.length; i++){
-            for (int k = 0; k < grid[i].length; k++){
-                sum += grid[i][k];
+        int max = 0;
+        for (int r = 0; r < twoDArray.length; r++){
+            for (int c = 0; c < twoDArray[r].length; c++){
+                sum += twoDArray[r][c];
+                if (twoDArray[r][c] > max) {
+                    max = twoDArray[r][c];
                 }
             }
-        System.out.println(sum);
         }
+        System.out.println("Sum of all elements: " + sum);
+        System.out.println("Maximum element: " + max);
     }
+}
