eb9db021b989996cc78a7c5e246f77648dd8c7da
Kerrie Liu <kerrie.liu@rollingrobotics.org>
2026-05-09 17:09:55 -0700

    archive java contd.

--- Java/Week4/Kerrie/StarPyramid.java
+++
@@ -1,8 +0,0 @@
-class StarPyramid {
-    public static void main(String[] args) {
-        int rows = 100; // you can replace 100 with any value you want
-        for (int i = 1; i < rows; i++) {
-            System.out.println("*".repeat(i));
-        }
-    }
-}
--- Java/Week6/Exercises.md
+++
@@ -1,4 +0,0 @@
-# Week 6 Review
-**Due on 5/9 by 3PM**
-## Review:
-Finish all sections available in [this course](https://www.codecademy.com/enrolled/courses/learn-java).
